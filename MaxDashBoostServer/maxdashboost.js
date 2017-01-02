d559a23d69746feb03f03c3eb03a45077c211b0e
asantaga <angelo@italianknows.com>
2017-01-02 19:55:33 +0000

    Added BlueBird Promise Library and serialized the boosting of radiators - No tested

--- MaxDashBoostServer/maxdashboost.js
+++ MaxDashBoostServer/maxdashboost.js
@@ -1,14 +1,17 @@
 var dash_button = require('node-dash-button');
 var MaxCube = require('maxcube');
 var config = require('./config.json');
-
+var promise=require('bluebird');
 
 // Radiators to Boost per set
-var boostSet1 = ["13856e", "13856e"];
+var boostSet1 = [
+    "13856e", "13856e"
+];
 
 console.log(config.maxCubeIp);
 
-var dashButtonList = [];
+var dashButtonList = [
+];
 for (i = 0; i < config.dashButtons.length; i++)
 {
     dashButtonList.push(config.dashButtons[i].id);
@@ -17,46 +20,52 @@ for (i = 0; i < config.dashButtons.length; i++)
 console.log('Listening to ' + i + " DashButtons " + dashButtonList);
 
 var dashButtonListener = dash_button(dashButtonList, null, null, 'all'); //address from step above
-dashButtonListener.on("detected", function (dash_id) {
+dashButtonListener.on("detected", function (dash_id)
+{
     // Button Pressed, find name of DashButton
-
-    var dashButtonData = config.dashButtons.filter(function (v) {
+    var dashButtonData = config.dashButtons.filter(function (v)
+    {
         return v.id === dash_id;
     })[0];
     console.log(dashButtonData);
     console.log(typeof dashButtonData);
-    if ( typeof dashButtonData === 'undefined' || dashButtonData === null )
+    if (typeof dashButtonData === 'undefined' || dashButtonData === null)
     {
         console.log("Unknown DashButton Found");
     }
     else
     {
-         console.log("Found Button "+dashButtonData.name+" boosting "+dashButtonData.boostRadiators);
-         boostRadiators(dashButtonData.boostRadiators);
+        console.log("Found Button " + dashButtonData.name + " boosting " + dashButtonData.boostRadiators);
+        boostRadiators(dashButtonData.boostRadiators);
     }
 
 });
 
 // Pass in a colleciton of radiators
 function boostRadiators(radiators) {
-   if ( typeof radiators === 'undefined' || radiators === null )
-        throw  "NoRadiators provided";
-    var myMaxCube = new MaxCube(config.maxCubeIp, 62910);
-    myMaxCube.on('connected', function () {
-        console.log('Connected to Max Cube');
-
-        for (var i = 0; i < radiators.length; i++) {
-            console.log("Boosting radiator " + radiators[i]);
-            myMaxCube.setTemperature(radiators[i], 'BOOST').then(
-                function (success) {
-                    console.log(JSON.stringify(success));
-                    console.log('Radiator '+radiators[i]+' BOOSTED');
-                },
-                function (error) {
-                    console.log('Error Boosting '+radiators[i]);
-                }
-             );
-        }
-    });
+if (typeof radiators === 'undefined' || radiators === null)
+{
+throw  "NoRadiators provided";
 }
+var myMaxCube = new MaxCube(config.maxCubeIp, 62910);
+myMaxCube.on('connected', 
+    function ()
+    {
+        console.log('Connected to Max Cube');
+        promise.each(radiators, 
+            function(element, index, length)
+            {
+                return  myMaxCube.setTemperature(element, 'BOOST').then(
+                    function (success)
+                    {
+                        console.log(JSON.stringify(success));
+                        console.log('Radiator ' + radiators[i] + ' BOOSTED');
+                    },
+                    function (error) 
+                    {
+                        console.log('Error Boosting ' + radiators[i]);
+                    });
+            });
+    });  
 console.log('Waiting for BoostButton Presses');
+
