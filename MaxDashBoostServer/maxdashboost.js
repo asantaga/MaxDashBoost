6994ab4c2ff155f79beaff85a5d06bbe232570f2
asantaga <angelo@italianknows.com>
2017-01-02 19:59:27 +0000

    Updated Readme.MD

--- MaxDashBoostServer/maxdashboost.js
+++ MaxDashBoostServer/maxdashboost.js
@@ -1,22 +1,21 @@
-var dash_button = require('node-dash-button');
-var MaxCube = require('maxcube');
-var config = require('./config.json');
-var promise=require('bluebird');
+var dash_button = require('node-dash-button');  // Node-Dash-Button library
+var MaxCube = require('maxcube');               // MaxCube Library
+var config = require('./config.json');          // config.json for properties file
+var promise = require('bluebird');              // Using Bluebird for Promises
 
 // Radiators to Boost per set
 var boostSet1 = [
     "13856e", "13856e"
 ];
 
-console.log(config.maxCubeIp);
-
+console.log("Connecting to MaxCube @ "+  config.maxCubeIp);
+// Print out DashButtons Being listened to
 var dashButtonList = [
 ];
 for (i = 0; i < config.dashButtons.length; i++)
 {
     dashButtonList.push(config.dashButtons[i].id);
 }
-
 console.log('Listening to ' + i + " DashButtons " + dashButtonList);
 
 var dashButtonListener = dash_button(dashButtonList, null, null, 'all'); //address from step above
@@ -42,30 +41,38 @@ dashButtonListener.on("detected", function (dash_id)
 });
 
 // Pass in a colleciton of radiators
-function boostRadiators(radiators) {
-if (typeof radiators === 'undefined' || radiators === null)
+function boostRadiators(radiators)
 {
-throw  "NoRadiators provided";
-}
-var myMaxCube = new MaxCube(config.maxCubeIp, 62910);
-myMaxCube.on('connected', 
-    function ()
+    if (typeof radiators === 'undefined' || radiators === null)
     {
-        console.log('Connected to Max Cube');
-        promise.each(radiators, 
-            function(element, index, length)
+        throw  "NoRadiators provided to boostRadiators";
+    }
+    console.log("Boosting Radiators "+JSON.stringify(radiators));
+    
+    var myMaxCube = new MaxCube(config.maxCubeIp, 62910);
+    myMaxCube.on('connected',
+            function ()
             {
-                return  myMaxCube.setTemperature(element, 'BOOST').then(
-                    function (success)
-                    {
-                        console.log(JSON.stringify(success));
-                        console.log('Radiator ' + radiators[i] + ' BOOSTED');
-                    },
-                    function (error) 
-                    {
-                        console.log('Error Boosting ' + radiators[i]);
-                    });
-            });
-    });  
+                console.log('Connected to Max Cube');
+                promise.each(radiators,
+                        function (element, index, length)
+                        {
+                            return  myMaxCube.setTemperature(element, 'BOOST').then(
+                                    function (success)
+                                    {
+                                        console.log(JSON.stringify(success));
+                                        console.log('Radiator ' + radiators[i] + ' BOOSTED');
+                                    },
+                                    function (error)
+                                    {
+                                        console.log('Error Boosting ' + radiators[i]);
+                                    });
+                        });
+                // Done Boosting close connection
+                myMaxCube.close().then (function() { console.log('Connection Closed');});                
+            }
+    );
+
+}
 console.log('Waiting for BoostButton Presses');
 
