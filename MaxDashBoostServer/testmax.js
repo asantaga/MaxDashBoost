47e8c800284f3215e11f2f65b3583bd662bc6ed3
Angelo Santagata <angelo@italianknows.com>
2017-03-14 23:54:30 +0000

    Added TestMax

---
+++ MaxDashBoostServer/testmax.js
@@ -0,0 +1,27 @@
+var MaxCube = require('maxcube');
+var myMaxCube = new MaxCube('192.168.0.222', 62910);
+
+myMaxCube.on('connected', function () {
+  console.log('Connected');
+    myMaxCube.setTemperature('1381f8',21).then(
+        function(success)
+        {
+          if (success)
+          {
+            console.log('Temperature set');
+          }
+          else {
+            console.log('Error setting Temperature');
+          }
+          myMaxCube.close();
+        },
+        function(failure)
+        {
+           console.log('Error occured '+failure);
+        } 
+        );
+  });
+
+myMaxCube.on('closed', function () {
+  console.log('Connection closed');
+});
